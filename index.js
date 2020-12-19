816e993b5d54923e7852bf72333690ef9caa3317
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-19 06:00:44 -0800

    Add db and model connection

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+require('./db/db');
+
 const express = require('express');
 
+const port = process.env.PORT || 3001;
+
 const app = express();
+
+app.use(express.json());
+
 app.use(express.static(__dirname));
 
-const server = app.listen(3000, () => {
-	console.log('server is running on port:', server.address().port);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	next();
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
